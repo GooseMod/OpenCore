2179bde48c7b35d2d49a76b58a393241c5a44134
Oj <oj@oojmed.com>
2021-12-29 20:14:53 +0000

    [Scripts > Test] Make home dir generic

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ echo "Packing asar..."
 asar pack src core.asar
 
 echo "Copying asar..."
-cp core.asar /home/goose/.config/discordcanary/0.0.131/modules/discord_desktop_core/core.asar
+cp core.asar ~/.config/discordcanary/0.0.131/modules/discord_desktop_core/core.asar
 
 echo "Running discord..."
 echo ""
